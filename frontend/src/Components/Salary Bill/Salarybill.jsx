f54e5e3317e8fa7d8db5353b9cdf1f5c4cf06aa4
Afsal7359 <afsal4771@gmail.com>
2023-11-22 17:13:03 +0530

    new commit

--- frontend/src/Components/Salary Bill/Salarybill.jsx
+++ frontend/src/Components/Salary Bill/Salarybill.jsx
@@ -1,60 +1,68 @@
 import React from 'react'
+import PageHeader from '../PageHeader'
+
+
+
 
 function Salarybill() {
+
+
   return (
     <>
-      <div className="page-header">
-					<div className="row">
-						<div className="col-sm-12">
-							<ul className="breadcrumb">
-								<li className="breadcrumb-item"><a href="index.html">Salary</a></li>
-								<li className="breadcrumb-item"><i className="feather-chevron-right"></i></li>
-								<li className="breadcrumb-item active">Salary Bill</li>
-							</ul>
-						</div>
-					</div>
-				</div>
+      <PageHeader/>
 
 
-
-				<div className="col-sm-12">
-						<div className="card">
-								<div className="card-body d-flex">
-									<div className="col-12 col-md-6 col-xl-4 d-inline-block m-3">
-												<div className="form-group local-forms">
-													<label>Country <span className="login-danger">*</span></label>
-													<input
-													
-													type="text"
-													className='form-control'
-													placeholder=""
-													
-													/>
-													
-												</div>
-											</div>
-											<div className="col-12 col-md-6 col-xl-4 d-inline-block m-3">
-												<div className="form-group local-forms">
-													<label>Country <span className="login-danger">*</span></label>
-													<input
-													
-													type="text"
-													className='form-control'
-													placeholder=""
-													
-													/>
-													
-												</div>
-											</div>
+							<form>
+							<div class="container mt-5">
+								<div class="row">
+									<div class="col-sm-4">
+									   <div className="form-group local-forms">
+											<label>Salary No <span className="login-danger">*</span></label>
+										    <input type="text" className="form-control" style={{backgroundColor:"#cbd0d6"}} readOnly/>
 										</div>
-								</div>
+									</div>
+									
+									<div class="col-sm-4">
+									   <div className="form-group local-forms">
+											<label>Date <span className="login-danger">*</span></label>
+										    <input type="date" className="form-control"  />
+										</div>
+									</div>
+									<div class="col-sm-4">
+									   <div className="form-group local-forms">
+											<label>Employee Name <span className="login-danger">*</span></label>
+										    <input type="search" className="form-control" placeholder='search....' />
+										
+										</div>
+									</div>
+									<div class="col-sm-4">
+									   <div className="form-group local-forms">
+											<label>Unit <span className="login-danger">*</span></label>
+										    <input type="text" className="form-control" style={{backgroundColor:"#cbd0d6"}} readOnly/>
+										</div>
+									</div>
+									<div class="col-sm-4">
+									   <div className="form-group local-forms">
+											<label>Department <span className="login-danger">*</span></label>
+										    <input type="text" className="form-control" style={{backgroundColor:"#cbd0d6"}}  readOnly/>
+										</div>
+									</div>
+									<div class="col-sm-4">
+									   <div className="form-group local-forms">
+											<label>Basic Salary <span className="login-danger">*</span></label>
+										    <input type="text" className="form-control" style={{backgroundColor:"#cbd0d6"}}  readOnly/>
+										</div>
+									</div>
+								</div>	
 							</div>
+
+				
 								<div className="row">
 											<div className="col-md-12">
 											<div className="card invoices-add-card">
 												<div className="card-body">
 													<div className="invoice-add-table">
-													<h4>Salary component</h4>
+													
 													<div className="table-responsive">
 														<table className="table table-striped table-nowrap  mb-0 no-footer add-table-items">
 														<thead>
@@ -78,7 +86,15 @@ function Salarybill() {
 															</tr>
 														</tbody>
 														<tfoot>
-															
+															<tr>
+																<td colSpan="4" className='text-end'>Allowed Leaves</td>
+																<td><input className="form-control" type="number" value={10} style={{backgroundColor:"#cbd0d6"}} readOnly/></td>
+															</tr>
+															<tr>
+																<td colSpan="4" className='text-end'>Absent</td>
+																<td><input className="form-control" type="number" /></td>
+															</tr>
+															<div style={{display:"none"}}>balance Leave</div>
 															<tr>
 															<td colSpan="4" className="text-end"><strong>Total Amount:</strong></td>
 															<td><input className="form-control" type="number" value={210} readOnly/>
@@ -89,14 +105,21 @@ function Salarybill() {
 															</tr>
 														</tfoot>
 														</table>
+														
 													</div>
+													<div className="col-12">
+											<div className="doctor-submit text-end m-3">
+												<button type="submit" className="btn btn-primary submit-form me-2">Submit</button>
+												<button type="submit" className="btn btn-primary cancel-form">Cancel</button>
+											</div>
+										</div>
 													</div>
 												</div>
 											</div>
 											</div>
 										</div>
-			
-				
+										
+										</form>
 
 
 
